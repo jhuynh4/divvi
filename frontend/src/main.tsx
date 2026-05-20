9fd8e83bb3a15a0c85579358e865400a3800f2c7
Joey Huynh <huynhjoey75@gmail.com>
2026-05-20 13:34:18 -0500

    Set up frontend routing and session API integration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {ChakraProvider, defaultSystem} from "@chakra-ui/react";
+import {
+    QueryClient,
+    QueryClientProvider
+} from "@tanstack/react-query"
+
 import './index.css'
 import App from './App.tsx'
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <QueryClientProvider client={queryClient}>
+          <ChakraProvider value={defaultSystem}>
+              <App/>
+          </ChakraProvider>
+      </QueryClientProvider>
   </StrictMode>,
 )
