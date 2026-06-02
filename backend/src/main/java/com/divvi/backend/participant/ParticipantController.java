ac79bdc46919619748089e9a89e6d01b43e1a465
Joey Huynh <huynhjoey75@gmail.com>
2026-06-02 15:46:22 -0500

    add remove participant option to session page

--- backend/src/main/java/com/divvi/backend/participant/ParticipantController.java
+++ backend/src/main/java/com/divvi/backend/participant/ParticipantController.java
@@ -2,7 +2,10 @@ package com.divvi.backend.participant;
 
 import com.divvi.backend.participant.dto.JoinSessionRequest;
 import com.divvi.backend.participant.dto.ParticipantResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/sessions")
@@ -19,5 +22,14 @@ public class ParticipantController {
             @RequestBody JoinSessionRequest request
     ) {
         return participantService.joinSession(shareCode, request);
+    }
+
+    @DeleteMapping("/{shareCode}/participants/{participantId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeParticipant(
+            @PathVariable String shareCode,
+            @PathVariable UUID participantId
+    ) {
+        participantService.removeParticipant(shareCode, participantId);
     }
 }
